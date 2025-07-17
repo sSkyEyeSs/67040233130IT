4892ca4177595399ebe1abb3d8e8542130d412c5
sSkyEyeSs <nonthawatng@gmail.com>
2025-07-17 15:55:49 +0700

    Add files via upload

---
+++ Labsql.sql
@@ -0,0 +1,6 @@
+Select * from Employees
+Select EmployeeID, FirstName, LastName From Employees
+Select * From Employees Where city = 'London'
+SELECT EmployeeID, FirstName, LastName From Employees Where city ='London'
+SELECT City, Country From Customers
+SELECT Distinct city, Country From Customers
